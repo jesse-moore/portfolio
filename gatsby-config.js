1afc5016989262b19ad21601fbc02e8548961636
Jesse <moore.jesse@gmail.com>
2020-12-08 10:00:37 -0600

    Add github pages plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jesse Moore`,
+    description: `Portfolio Site`,
+    author: `Jesse Moore`,
   },
+  pathPrefix: "/portfolio",
   plugins: [
     `gatsby-transformer-json`,
     {
